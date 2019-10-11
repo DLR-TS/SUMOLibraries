4b1ff54a4758ad2af14a490d7f0500605e759cd8
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2019-10-11 14:09:54 +0200

    Retargeting projects (C:/Libraries -> C:/SUMOLibraries) (II)

--- proj-6.1.1/man/cmake_install.cmake
+++ proj-6.1.1/man/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:\Libraries\proj-6.1.1")
+  set(CMAKE_INSTALL_PREFIX "C:\SUMOLibraries\proj-6.1.1")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
