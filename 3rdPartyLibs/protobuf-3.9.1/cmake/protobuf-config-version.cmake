9d16109c6de6254de28189ceefea438569985c5e
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2019-10-16 12:31:48 +0200

    Recompiled protobuf-3.9.1

--- 3rdPartyLibs/protobuf-3.9.1/cmake/protobuf-config-version.cmake
+++ 3rdPartyLibs/protobuf-3.9.1/cmake/protobuf-config-version.cmake
@@ -45,7 +45,7 @@ macro(_check_and_save_build_option OPTION VALUE)
   set(${PACKAGE_FIND_NAME}_${OPTION} ${VALUE} PARENT_SCOPE)
 endmacro()
 _check_and_save_build_option(WITH_ZLIB ON)
-_check_and_save_build_option(MSVC_STATIC_RUNTIME ON)
+_check_and_save_build_option(MSVC_STATIC_RUNTIME OFF)
 _check_and_save_build_option(BUILD_SHARED_LIBS OFF)
 
 # if the installed or the using project don't have CMAKE_SIZEOF_VOID_P set, ignore it:
