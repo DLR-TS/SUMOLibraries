cffaf774f7ef09b847e7f111b14072e626ca130f
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2021-01-04 11:18:20 +0100

    Removed xerces-c-3.2.2 (32 and 64 bits)

--- 32bits/xerces-c-3.2.2/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/SUMOLibraries/32bits/xerces-c-3.2.2/src
-# Build directory: C:/SUMOLibraries/32bits/xerces-c-3.2.2/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
