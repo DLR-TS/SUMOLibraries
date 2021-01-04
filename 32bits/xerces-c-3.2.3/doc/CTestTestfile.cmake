99846c31974f62f05910b9a86e2853e9b4a75902
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2021-01-04 15:11:49 +0100

    removed doc files

--- 32bits/xerces-c-3.2.3/doc/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Librerias/SUMOLibraries/32bits/xerces-c-3.2.3/doc
-# Build directory: C:/Librerias/SUMOLibraries/32bits/xerces-c-3.2.3/doc
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
