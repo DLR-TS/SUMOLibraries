43f783d05270c36a192397cedcc07b7b19db58cd
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2021-01-21 15:03:32 +0100

    recompiled freeType (as dynamic) (32 bits)

--- 32bits/freetype-2.10.4/lib/cmake/freetype/freetype-config.cmake
+++ 32bits/freetype-2.10.4/lib/cmake/freetype/freetype-config.cmake
@@ -51,16 +51,11 @@ if(_IMPORT_PREFIX STREQUAL "/")
 endif()
 
 # Create imported target freetype
-add_library(freetype STATIC IMPORTED)
+add_library(freetype SHARED IMPORTED)
 
 set_target_properties(freetype PROPERTIES
   INTERFACE_INCLUDE_DIRECTORIES "${_IMPORT_PREFIX}/include/freetype2"
-  INTERFACE_LINK_LIBRARIES "\$<\$<NOT:\$<CONFIG:DEBUG>>:C:/SUMOLibraries/3rdPartyLibs/zlib-1.2.11/lib/zlib.lib>;\$<\$<CONFIG:DEBUG>:C:/SUMOLibraries/3rdPartyLibs/zlib-1.2.11/lib/zlibd.lib>;\$<\$<NOT:\$<CONFIG:DEBUG>>:C:/SUMOLibraries/3rdPartyLibs/bzip2-1.0.6/lib/libbz2.lib>;\$<\$<CONFIG:DEBUG>:C:/SUMOLibraries/3rdPartyLibs/bzip2-1.0.6/lib/libbz2d.lib>;\$<\$<NOT:\$<CONFIG:DEBUG>>:C:/SUMOLibraries/3rdPartyLibs/libpng-1.6.34/lib/libpng16.lib>;\$<\$<CONFIG:DEBUG>:C:/SUMOLibraries/3rdPartyLibs/libpng-1.6.34/lib/libpng16d.lib>;\$<\$<NOT:\$<CONFIG:DEBUG>>:C:/SUMOLibraries/3rdPartyLibs/zlib-1.2.11/lib/zlib.lib>;\$<\$<CONFIG:DEBUG>:C:/SUMOLibraries/3rdPartyLibs/zlib-1.2.11/lib/zlibd.lib>"
 )
-
-if(CMAKE_VERSION VERSION_LESS 2.8.12)
-  message(FATAL_ERROR "This file relies on consumers using CMake 2.8.12 or greater.")
-endif()
 
 # Load information for each installed configuration.
 get_filename_component(_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
