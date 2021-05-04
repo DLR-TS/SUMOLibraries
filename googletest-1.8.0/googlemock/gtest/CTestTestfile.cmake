7773f48282c55e7368ce8b16035caa3ea49bcf85
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2021-05-04 15:34:44 +0200

    Release 1.10.0

--- googletest-1.8.0/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/SUMOLibraries/googletest-master/googletest
-# Build directory: C:/SUMOLibraries/googletest-master/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
