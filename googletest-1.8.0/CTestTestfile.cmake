044b29846912d969fbfe4fb2f6e644514302353a
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2021-05-04 16:18:32 +0200

    removed google test 1.8.0

--- googletest-1.8.0/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/SUMOLibraries/googletest-master
-# Build directory: C:/SUMOLibraries/googletest-master
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
