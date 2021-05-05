2c159c54fb62b089a5491a36d7531ba2a9a44923
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2021-05-05 11:23:51 +0200

    Recompiled googletest-1.10.0

--- googletest-1.10.0/lib/cmake/GTest/GTestTargets.cmake
+++ googletest-1.10.0/lib/cmake/GTest/GTestTargets.cmake
@@ -51,33 +51,37 @@ if(_IMPORT_PREFIX STREQUAL "/")
 endif()
 
 # Create imported target GTest::gtest
-add_library(GTest::gtest STATIC IMPORTED)
+add_library(GTest::gtest SHARED IMPORTED)
 
 set_target_properties(GTest::gtest PROPERTIES
+  INTERFACE_COMPILE_DEFINITIONS "GTEST_LINKED_AS_SHARED_LIBRARY=1"
   INTERFACE_INCLUDE_DIRECTORIES "${_IMPORT_PREFIX}/include"
   INTERFACE_SYSTEM_INCLUDE_DIRECTORIES "${_IMPORT_PREFIX}/include"
 )
 
 # Create imported target GTest::gtest_main
-add_library(GTest::gtest_main STATIC IMPORTED)
+add_library(GTest::gtest_main SHARED IMPORTED)
 
 set_target_properties(GTest::gtest_main PROPERTIES
+  INTERFACE_COMPILE_DEFINITIONS "GTEST_LINKED_AS_SHARED_LIBRARY=1"
   INTERFACE_INCLUDE_DIRECTORIES "${_IMPORT_PREFIX}/include"
   INTERFACE_SYSTEM_INCLUDE_DIRECTORIES "${_IMPORT_PREFIX}/include"
 )
 
 # Create imported target GTest::gmock
-add_library(GTest::gmock STATIC IMPORTED)
+add_library(GTest::gmock SHARED IMPORTED)
 
 set_target_properties(GTest::gmock PROPERTIES
+  INTERFACE_COMPILE_DEFINITIONS "GTEST_LINKED_AS_SHARED_LIBRARY=1"
   INTERFACE_INCLUDE_DIRECTORIES "${_IMPORT_PREFIX}/include"
   INTERFACE_SYSTEM_INCLUDE_DIRECTORIES "${_IMPORT_PREFIX}/include"
 )
 
 # Create imported target GTest::gmock_main
-add_library(GTest::gmock_main STATIC IMPORTED)
+add_library(GTest::gmock_main SHARED IMPORTED)
 
 set_target_properties(GTest::gmock_main PROPERTIES
+  INTERFACE_COMPILE_DEFINITIONS "GTEST_LINKED_AS_SHARED_LIBRARY=1"
   INTERFACE_INCLUDE_DIRECTORIES "${_IMPORT_PREFIX}/include"
   INTERFACE_SYSTEM_INCLUDE_DIRECTORIES "${_IMPORT_PREFIX}/include"
 )
