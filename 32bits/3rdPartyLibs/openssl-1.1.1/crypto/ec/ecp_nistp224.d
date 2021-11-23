34b4d5865f1847644ebe94db7f437c0a0a96d024
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2021-11-23 11:06:12 +0100

    removed 32 bits libraries

--- 32bits/3rdPartyLibs/openssl-1.1.1/crypto/ec/ecp_nistp224.d
+++
@@ -1,3 +0,0 @@
-ecp_nistp224.c
-Note: including file: C:\Librerias\SUMOLibraries\32bits\3rdPartyLibs\openssl-1.1.1\include\openssl/opensslconf.h
-Note: including file:  C:\Librerias\SUMOLibraries\32bits\3rdPartyLibs\openssl-1.1.1\include\openssl/opensslv.h
--- 32bits/3rdPartyLibs/openssl-1.1.1/crypto/rand/rand_egd.d
+++
@@ -1,3 +0,0 @@
-rand_egd.c
-Note: including file: C:\Librerias\SUMOLibraries\32bits\3rdPartyLibs\openssl-1.1.1\include\openssl/opensslconf.h
-Note: including file:  C:\Librerias\SUMOLibraries\32bits\3rdPartyLibs\openssl-1.1.1\include\openssl/opensslv.h
