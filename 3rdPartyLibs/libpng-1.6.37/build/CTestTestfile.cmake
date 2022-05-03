a0c481d039e3ea9ec12da6167c697a134b1ae210
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2022-05-03 13:48:38 +0200

    added libpng-1.6.37

---
+++ 3rdPartyLibs/libpng-1.6.37/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Librerias/SUMOLibraries/3rdPartyLibs/libpng-1.6.37
+# Build directory: C:/Librerias/SUMOLibraries/3rdPartyLibs/libpng-1.6.37/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
