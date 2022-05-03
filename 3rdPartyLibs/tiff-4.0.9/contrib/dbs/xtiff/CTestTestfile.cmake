5de1c078db25ed95af3e02a0c0e24b33ef965b08
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2022-05-03 15:54:04 +0200

    updated folders

--- 3rdPartyLibs/tiff-4.0.9/contrib/dbs/xtiff/CTestTestfile.cmake
+++ 3rdPartyLibs/tiff-4.0.9/contrib/dbs/xtiff/CTestTestfile.cmake
@@ -1,6 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: C:/SUMOLibraries/3rdPartyLibs/tiff-4.0.9/contrib/dbs/xtiff
-# Build directory: C:/SUMOLibraries/3rdPartyLibs/tiff-4.0.9/contrib/dbs/xtiff
+# Source directory: C:/Librerias/SUMOLibraries/3rdPartyLibs/tiff-4.0.9/contrib/dbs/xtiff
+# Build directory: C:/Librerias/SUMOLibraries/3rdPartyLibs/tiff-4.0.9/contrib/dbs/xtiff
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
--- 3rdPartyLibs/tiff-4.0.9/man/CTestTestfile.cmake
+++ 3rdPartyLibs/tiff-4.0.9/man/CTestTestfile.cmake
@@ -1,6 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: C:/SUMOLibraries/3rdPartyLibs/tiff-4.0.9/man
-# Build directory: C:/SUMOLibraries/3rdPartyLibs/tiff-4.0.9/man
+# Source directory: C:/Librerias/SUMOLibraries/3rdPartyLibs/tiff-4.0.9/man
+# Build directory: C:/Librerias/SUMOLibraries/3rdPartyLibs/tiff-4.0.9/man
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
