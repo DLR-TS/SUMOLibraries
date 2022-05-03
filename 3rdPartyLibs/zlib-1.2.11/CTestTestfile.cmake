976c3c1f8747d640df6c5859f01f8063e010138e
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2022-05-03 11:26:51 +0200

    removed zlib-1.2.11

--- 3rdPartyLibs/zlib-1.2.11/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/SUMOLibraries/3rdPartyLibs/zlib-1.2.11
-# Build directory: C:/SUMOLibraries/3rdPartyLibs/zlib-1.2.11
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(example "example")
-set_tests_properties(example PROPERTIES  _BACKTRACE_TRIPLES "C:/SUMOLibraries/3rdPartyLibs/zlib-1.2.11/CMakeLists.txt;235;add_test;C:/SUMOLibraries/3rdPartyLibs/zlib-1.2.11/CMakeLists.txt;0;")
