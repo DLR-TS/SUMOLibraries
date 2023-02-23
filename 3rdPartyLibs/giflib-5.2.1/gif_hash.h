ddc69031ec40eba4e2b17022a00995666c2516dc
Pablo Alvarez lopez <pablo.alvarezlopez@dlr.de>
2023-02-23 07:59:13 +0100

    compiled giflib-5.2.1

--- 3rdPartyLibs/giflib-5.2.1/gif_hash.h
+++ 3rdPartyLibs/giflib-5.2.1/gif_hash.h
@@ -9,7 +9,9 @@ SPDX-License-Identifier: MIT
 #ifndef _GIF_HASH_H_
 #define _GIF_HASH_H_
 
+#ifndef _WIN32
 #include <unistd.h>
+#endif
 #include <stdint.h>
 
 #define HT_SIZE			8192	   /* 12bits = 4096 or twice as big! */
