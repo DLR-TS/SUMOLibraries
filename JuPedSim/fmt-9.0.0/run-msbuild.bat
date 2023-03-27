b1b10d91fdf297fb1e26afa8d44663980939356e
Michael Behrisch <michael.behrisch@dlr.de>
2023-03-27 13:50:35 +0200

    adding compiled jupedsim

--- JuPedSim/fmt-9.0.0/run-msbuild.bat
+++
@@ -1,3 +0,0 @@
-
-    
-    C:/Microsoft Visual Studio/2022/Community/MSBuild/Current/Bin/amd64/MSBuild.exe -p:FrameworkPathOverride="C:\Program Files\Reference Assemblies\Microsoft\Framework;.NETFramework\v4.0" %*
