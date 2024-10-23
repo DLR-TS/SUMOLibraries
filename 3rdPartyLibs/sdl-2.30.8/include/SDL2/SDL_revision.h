570d546754c7192de6c762e53c628e75dbfcbdf1
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2024-10-23 08:50:17 +0200

    SDL

---
+++ 3rdPartyLibs/sdl-2.30.8/include/SDL2/SDL_revision.h
@@ -0,0 +1,8 @@
+/* #undef SDL_VENDOR_INFO */
+#define SDL_REVISION_NUMBER 0
+
+#ifdef SDL_VENDOR_INFO
+#define SDL_REVISION "SDL-2.30.8-g3a491ec3b (" SDL_VENDOR_INFO ")"
+#else
+#define SDL_REVISION "SDL-2.30.8-g3a491ec3b"
+#endif
