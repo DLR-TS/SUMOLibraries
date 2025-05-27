bee92ef5460543290aecd3c672e9df45e7a172f2
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2025-05-27 10:18:39 +0200

    added minizip-ng-4.0.10 (compiled static without LZMA and zstd)

---
+++ 3rdPartyLibs/minizip-ng-4.0.10/lib/cmake/minizip/minizip-release.cmake
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "MINIZIP::minizip" for configuration "Release"
+set_property(TARGET MINIZIP::minizip APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(MINIZIP::minizip PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/minizip.lib"
+  )
+
+list(APPEND _cmake_import_check_targets MINIZIP::minizip )
+list(APPEND _cmake_import_check_files_for_MINIZIP::minizip "${_IMPORT_PREFIX}/lib/minizip.lib" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
