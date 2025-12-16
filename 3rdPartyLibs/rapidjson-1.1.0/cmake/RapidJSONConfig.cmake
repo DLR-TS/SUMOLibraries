a71441cce51dea77cabe135ce010b1863f4a4700
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2025-12-16 12:04:38 +0100

    rapidjson-1.1.0

---
+++ 3rdPartyLibs/rapidjson-1.1.0/cmake/RapidJSONConfig.cmake
@@ -0,0 +1,3 @@
+get_filename_component(RAPIDJSON_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
+set(RAPIDJSON_INCLUDE_DIRS "D:/SUMOLibraries/3rdPartyLibs/rapidjson-1.1.0/include")
+message(STATUS "RapidJSON found. Headers: ${RAPIDJSON_INCLUDE_DIRS}")
